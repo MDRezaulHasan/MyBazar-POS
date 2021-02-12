52204ee5b3650e73db1de64a1758812e6cd1271d
MDRezaulHasan <rezaulhasan0168@gmail.com>
2021-02-12 12:34:36 +0100

    Reset password and manage db error

--- MyBazarPOSApp/frmLogin.Designer.cs
+++ MyBazarPOSApp/frmLogin.Designer.cs
@@ -69,9 +69,9 @@ namespace MyBazarPOSApp
             // 
             this.txtPassword.Location = new System.Drawing.Point(172, 170);
             this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(274, 20);
             this.txtPassword.TabIndex = 4;
-            this.txtPassword.Text = "\r\n";
             this.txtPassword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label2
